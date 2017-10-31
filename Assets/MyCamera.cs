5b7705e045f1737b5e8bf41734259944500714e0
Naoki Nakagawa <xxvraqwa@gmail.com>
2017-10-31 15:07:41 +0900

    カメラクラスを作成。警告が出たのでクラス名は変えずにファイル名だけ変えた

---
+++ Assets/MyCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity = UnityEngine;
+
+namespace OnlyScript
+{
+    public class Camera : Unity.MonoBehaviour
+    {
+        void Awake()
+        {
+            transform.position = new Unity.Vector3(0f, 0f, -10f);
+
+            Unity.Camera c = gameObject.AddComponent<Unity.Camera>();
+            c.clearFlags = Unity.CameraClearFlags.SolidColor;
+            c.backgroundColor = new Unity.Color(0.5f, 0.75f, 1f);
+            c.nearClipPlane = 0.1f;
+            c.farClipPlane = 100f;
+        }
+    }
+}
