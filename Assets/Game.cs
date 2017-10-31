d89ea4f085479c9b14eed40cd9476be695fe0043
Naoki Nakagawa <xxvraqwa@gmail.com>
2017-10-31 15:06:49 +0900

    Gameクラス作成 シーンとか使うときは管理する シーン使わないときはゲームオブジェクトを管理する 要はエディタのヒエラルキーの役割

---
+++ Assets/Game.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity = UnityEngine;
+
+namespace OnlyScript
+{
+    public class Game : Unity.MonoBehaviour
+    {
+        [Unity.RuntimeInitializeOnLoadMethod]
+        static void Initialize()
+        {
+            new Unity.GameObject("Game", typeof(OnlyScript.Game));
+        }
+
+        IEnumerator Start()
+        {
+            new Unity.GameObject("Camera", typeof(OnlyScript.Camera));
+
+            while (true)
+            {
+                new Unity.GameObject("Character", typeof(OnlyScript.Character));
+                yield return null;
+            }
+        }
+    }
+}
